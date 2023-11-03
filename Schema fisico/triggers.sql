b5e5e930b49d6aec073fa0c965e8310eec6bed89
Rob <saiellorobertorosario@gmail.com>
2023-11-03 11:55:31 +0100

    fix del trigger per inserimento luogo tramite foto

--- Schema fisico/triggers.sql
+++ Schema fisico/triggers.sql
@@ -1,16 +1,18 @@
---trigger che permette di caricare luoghi tramite foto
-CREATE OR REPLACE FUNCTION galleria.check_luogo_fn()
+--trigger che permette di caricare luoghi solo tramite foto
+CREATE OR REPLACE FUNCTION a_check_luogo_fn()
 RETURNS TRIGGER
 AS $$
 DECLARE 
-    presenza INTEGER; --una variabile di tipo intero che conterra' 1, se il luogo e' presente, e 0 se non lo e'.
+    presenza INTEGER; --una variabile di tipo intero che conterra' 0, se il luogo non e' presente, e != 0 se lo e'.
 BEGIN
-    SELECT COUNT(*) into presenza
-    FROM galleria.LUOGO
-    WHERE galleria.LUOGO.Coordinate = NEW.Coordinate;
+    SELECT COUNT(*) INTO presenza
+    FROM LUOGO
+    WHERE LUOGO.Coordinate = NEW.Coordinate;
     
-    IF presenza = 0 THEN
-        INSERT INTO galleria.LUOGO(coordinate, tiponome) VALUES (NEW.Coordinate, NULL);
+    IF presenza = 0 THEN 
+        IF NEW.Coordinate IS NOT NULL THEN
+            INSERT INTO LUOGO(coordinate) VALUES (NEW.Coordinate);
+        END IF;
     ELSE
         RAISE EXCEPTION 'Non è possibile inserire le coordinate (%) dato che sono già presenti.', NEW.Coordinate;
     END IF;
@@ -20,22 +22,22 @@ END;
 $$
 LANGUAGE PLPGSQL;
 
-CREATE OR REPLACE TRIGGER check_luogo_tr
+CREATE OR REPLACE TRIGGER a_check_luogo_tr
 BEFORE INSERT 
-ON galleria.FOTO
+ON FOTO
 FOR EACH ROW
-EXECUTE FUNCTION galleria.check_luogo_fn();
+EXECUTE FUNCTION a_check_luogo_fn();
 
 --------------------------------------------------------------------------------------------------------------------------
 
 --trigger che non permette l'aggiornamento della data di eliminazione a null 
-CREATE OR REPLACE FUNCTION galleria.check_data_eliminazione_fn()
+CREATE OR REPLACE FUNCTION check_data_eliminazione_fn()
 RETURNS TRIGGER
 AS $$
 BEGIN    
     --La data di eliminazione non puo' essere aggiornata, dato che la foto e' gia' stata eliminata
     --La data di eliminazione non puo' essere messa a NULL una volta che e' ne stata inserita una
-    IF NEW.DataEliminazione IS NULL THEN
+    IF NEW.DataEliminazione <> OLD.DataEliminazione OR NEW.DataEliminazione IS NULL THEN
         RAISE EXCEPTION 'La data di eliminazione non è aggiornabile, in quanto la foto è già stata eliminata';
     END IF;
     
@@ -46,9 +48,9 @@ LANGUAGE PLPGSQL;
 
 CREATE OR REPLACE TRIGGER check_data_eliminazione_tr
 BEFORE UPDATE 
-ON galleria.FOTO
+ON FOTO
 FOR EACH ROW
-EXECUTE FUNCTION galleria.check_data_eliminazione_fn();
+EXECUTE FUNCTION check_data_eliminazione_fn();
 
 --------------------------------------------------------------------------------------------------------------------------
 
