82980b25fc75e4604068453e18a285e7f3afbdd3
Rob <saiellorobertorosario@gmail.com>
2023-11-09 10:19:39 +0100

    Aggiunta di un dominio dedicato alle stringe generiche di tipo VARCHAR(50).

--- Schema fisico/domini.sql
+++ Schema fisico/domini.sql
@@ -14,3 +14,5 @@ CREATE DOMAIN galleria.id_object_dt CHAR(10)
 --dominio dedicato alle coordinate, espresse in modo decimale, con una precisione fino a 2 decimali.
 CREATE DOMAIN galleria.coo_dt CHAR(14)
     CONSTRAINT coordinate_format CHECK (VALUE ~ '^[+-][0-9]{2}\.[0-9]{2},[+-][0-9]{3}\.[0-9]{2}$');
+
+CREATE DOMAIN galleria.string VARCHAR(50);
