a9b36954ddf1150411dbacb9afbd492bfd96d72a
Rob <saiellorobertorosario@gmail.com>
2023-11-14 12:37:15 +0100

    Creazione file views
    File dedito a mantenere tutte le views della base di dati.
    Attualmente contiene solo la view per la top 3 luoghi piu' immortalati.

---
+++ Schema fisico/views.sql
@@ -0,0 +1,6 @@
+.CREATE VIEW top_luoghi_w AS 
+SELECT COUNT(*), F.Coordinate
+FROM galleria.FOTO F JOIN galleria.LUOGO L ON F.Coordinate = L.Coordinate
+GROUP BY F.Coordinate
+ORDER BY COUNT (*) DESC 
+LIMIT 3;
