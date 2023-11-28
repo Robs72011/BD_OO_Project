c18184c47c4099363168af7638ddcc42b861fd7b
GiovanniSpoleto <118965303+GiovanniSpoleto@users.noreply.github.com>
2023-11-28 13:44:07 +0100

    Update views.sql
    CORREZZIONI

--- Schema fisico/views.sql
+++ Schema fisico/views.sql
@@ -1,6 +1,6 @@
-CREATE VIEW galleria.top_luoghi_w AS 
-SELECT COUNT(*), F.Coordinate
+CREATE OR REPLACE VIEW galleria.top_luoghi_w AS 
+SELECT COUNT(*), L.Coordinate, L.toponimo
 FROM galleria.FOTO F JOIN galleria.LUOGO L ON F.Coordinate = L.Coordinate
-GROUP BY F.Coordinate
+GROUP BY L.Coordinate
 ORDER BY COUNT (*) DESC 
 LIMIT 3;
